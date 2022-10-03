5b780d316b75a45365bc5740aeb218fd585543a3
Naresh SinghGehlot <81186869+98nareshinn@users.noreply.github.com>
2022-10-02 23:56:46 -0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,57 @@
+const products = [
+    {
+      id: 0,
+      name: "AFFOGATO",
+      price: 29.99,
+      instock: 100,
+      description:
+        "Espresso poured on a vanilla ice cream. Served in a cappuccino cup.",
+      imgSrc: "./img/t1.png",
+    },
+    {
+      id: 1,
+      name: "CAFFÈ MOCHA",
+      price: 24.99,
+      instock: 43,
+      description:
+        "A caffè latte with chocolate and whipped cream.",
+      imgSrc: "./img/t2.png",
+    },
+    {
+      id: 2,
+      name: "CAFÈ AU LAIT",
+      price: 19.99,
+      instock: 10,
+      description:
+        "French morning coffee. Made by mixing dark roasted filter coffee.",
+      imgSrc: "./img/t3.png",
+    },
+    {
+      id: 3,
+      name: "CAPPUCCINO",
+      price: 25.99,
+      instock: 5,
+      description:
+        "A coffee drink consisting of espresso and a milk foam mixture.",
+      imgSrc: "./img/t4.png",
+    },
+    {
+      id: 4,
+      name: "IRISH COFFEE",
+      price: 29.99,
+      instock: 4,
+      description:
+        "Classic coffee coctail where Irish whiskey is mixed with filter coffee.",
+      imgSrc: "./img/t5.png",
+    },
+    {
+      id: 5,
+      name: "ESH.. MACCHIATO",
+      price: 39.99,
+      instock: 40,
+      description:
+        "Espresso with a little milk foam.Served in an espresso cup.",
+      imgSrc: "./img/t6.png",
+    },
+  ];
+  
